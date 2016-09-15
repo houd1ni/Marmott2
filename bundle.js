1274253e898bcef06ae8c122e61a9a5f7e2df060
Michael <pinke@live.ru>
2016-09-15 22:12:16 +0300

    build

--- bundle.js
+++ bundle.js
@@ -1,7 +1,69 @@
 var XHR;
+var getf;
+var w;
+
+getf = function(fn) {
+  if (fn) {
+    return fn;
+  } else {
+    return function() {};
+  }
+};
+
+w = window;
+
+
+/*
+  
+    XHR Transport
+
+    takes standart
+
+       * plain response. err: 0: no, 1: 404, 2: undef
+      cb: (err, response)->
+       * Parameters in an any object
+      p: {}
+       * method
+      m: `string` || 'POST'
+      u: `url` || '.'
+      h: `headers { name: value, name2: value2, ... }`
+ */
 
 XHR = (function() {
-  function XHR() {}
+  var default_headers;
+
+  default_headers = {
+    'X-Requested-With': ' XMLHttpRequest'
+  };
+
+  function XHR(O) {
+    var callback, headers, name, t, value;
+    t = this;
+    callback = getf(t.cb);
+    headers = O.h || {};
+    t.xhr = new (w.XMLHttpRequest || ActiveXObject('Microsoft.XMLHTTP'));
+    t.xhr.onreadystatechange = function() {
+      var e, response, state, status;
+      e = this;
+      state = e.readyState;
+      status = e.status;
+      response = e.responseText;
+      if (state === 4) {
+        code === ((199 < status && status < 207) ? 0 : +(!!(status === 404)) + 1);
+        return cb(code, response);
+      }
+    };
+    t.xhr.open(O.m || 'post', O.u || '.', true);
+    for (name in default_headers) {
+      value = default_headers[name];
+      headers[name] = v || default_headers[name];
+    }
+    for (name in headers) {
+      value = headers[name];
+      t.xhr.setRequestHeader(name, value);
+    }
+    t.xhr.send(O.p || null);
+  }
 
   return XHR;
 
